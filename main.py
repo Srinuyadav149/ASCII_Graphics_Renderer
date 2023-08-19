2b001f816206871ca533236ba756cd099fbc4955
Vurukuti Sai Srinivas <70220557+Srinuyadav149@users.noreply.github.com>
2023-08-20 00:34:34 +0530

    Initial Commit : Created a point module

---
+++ main.py
@@ -0,0 +1,3 @@
+from renderer.shapes.point import *
+
+print(Point2D(5,6))
---
+++ renderer/shapes/point.py
@@ -0,0 +1,32 @@
+class Point1D:
+    def __init__(self,x:float=0.0) -> None:
+        self.x:float = float(x)
+    
+    def __str__(self) -> str:
+        return f"Point1D{{x:{self.x}}}"
+    
+    def __repr__(self) -> str:
+        return f"Point1D(class){{x(float):{self.x}}}"
+    
+class Point2D:
+    def __init__(self,x:float=0.0,y:float=0.0) -> None:
+        self.x:float = float(x)
+        self.y:float = float(y)
+    
+    def __str__(self) -> str:
+        return f"Point2D{{x:{self.x},y:{self.y}}}"
+    
+    def __repr__(self) -> str:
+        return f"Point2D(class){{x(float):{self.x},y(float):{self.y}}}"
+
+class Point3D:
+    def __init__(self,x:float=0.0,y:float=0.0,z:float=0.0) -> None:
+        self.x:float = float(x)
+        self.y:float = float(y)
+        self.z:float = float(z)
+
+    def __str__(self) -> str:
+        return f"Point3D{{x:{self.x},y:{self.y},z:{self.z}}}"
+    
+    def __repr__(self) -> str:
+        return f"Point3D(class){{x:float{self.x},y:float{self.y},z:float{self.z}}}"
