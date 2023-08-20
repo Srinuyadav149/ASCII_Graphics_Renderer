8a88e9b42d6931811184002d95e4a37bde06255d
Vurukuti Sai Srinivas <70220557+Srinuyadav149@users.noreply.github.com>
2023-08-20 14:52:44 +0530

    done

--- renderer/shapes/point.py
+++ renderer/shapes/point.py
@@ -7,7 +7,7 @@ class Point1D:
     
     def __repr__(self) -> str:
         return f"Point1D(class){{x(float):{self.x}}}"
-    
+ 
 class Point2D:
     def __init__(self,x:float=0.0,y:float=0.0) -> None:
         self.x:float = float(x)
@@ -30,3 +30,16 @@ class Point3D:
     
     def __repr__(self) -> str:
         return f"Point3D(class){{x:float{self.x},y:float{self.y},z:float{self.z}}}"
+
+class Point4D:
+    def __init__(self,x:float=0.0,y:float=0.0,z:float=0.0,w:float=0.0) -> None:
+        self.x:float = float(x)
+        self.y:float = float(y)
+        self.z:float = float(z)
+        self.w:float = float(w)
+
+    def __str__(self) -> str:
+        return f"Point4D{{x:{self.x},y:{self.y},z:{self.z},w:{self.w}}}"
+    
+    def __repr__(self) -> str:
+        return f"Point4D(class){{x(float):{self.x},y(float):{self.y},z(float):{self.z},w(float):{self.w}}}"
